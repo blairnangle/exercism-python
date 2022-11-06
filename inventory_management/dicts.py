424830c3ea53010d54e4be05eb5df689ff8de03e
Blair Nangle <hi@blairnangle.com>
2022-11-06 22:34:57 +0000

    Complete "Inventory Management"

---
+++ inventory_management/dicts.py
@@ -0,0 +1,55 @@
+"""Functions to keep track and alter inventory."""
+from collections import Counter
+
+
+def create_inventory(items):
+    """Create a dict that tracks the amount (count) of each element on the `items` list.
+
+    :param items: list - list of items to create an inventory from.
+    :return: dict - the inventory dictionary.
+    """
+
+    return {item: items.count(item) for item in items}
+
+
+def add_items(inventory, items):
+    """Add or increment items in inventory using elements from the items `list`.
+
+    :param inventory: dict - dictionary of existing inventory.
+    :param items: list - list of items to update the inventory with.
+    :return: dict - the inventory updated with the new items.
+    """
+
+    return Counter(inventory) + Counter(create_inventory(items))
+
+
+def decrement_items(inventory, items):
+    """Decrement items in inventory using elements from the `items` list.
+
+    :param inventory: dict - inventory dictionary.
+    :param items: list - list of items to decrement from the inventory.
+    :return: dict - updated inventory with items decremented.
+    """
+
+    return {item: max(0, inventory[item] - items.count(item)) for item in inventory.keys()}
+
+
+def remove_item(inventory, item):
+    """Remove item from inventory if it matches `item` string.
+
+    :param inventory: dict - inventory dictionary.
+    :param item: str - item to remove from the inventory.
+    :return: dict - updated inventory with item removed. Current inventory if item does not match.
+    """
+
+    return {inventory_item: inventory[inventory_item] for inventory_item in inventory if inventory_item != item}
+
+
+def list_inventory(inventory):
+    """Create a list containing all (item_name, item_count) pairs in inventory.
+
+    :param inventory: dict - an inventory dictionary.
+    :return: list of tuples - list of key, value pairs from the inventory dictionary.
+    """
+
+    return list(filter(lambda item_count: item_count[1] > 0, [(item, inventory[item]) for item in inventory]))
