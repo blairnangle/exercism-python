6391d01a75d33f44856915f84d4177e2684f7118
Blair Nangle <hi@blairnangle.com>
2022-11-09 15:05:39 +0000

    Complete "Darts"

---
+++ darts/darts.py
@@ -0,0 +1,13 @@
+from math import sqrt
+
+
+def score(x, y):
+    distance_from_center = sqrt(x ** 2 + y ** 2)
+    if 0 <= distance_from_center <= 1:
+        return 10
+    elif 1 < distance_from_center <= 5:
+        return 5
+    elif 5 < distance_from_center <= 10:
+        return 1
+
+    return 0
