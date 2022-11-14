db4965b3755ea4eed323a326ecca568caf884f83
Blair Nangle <hi@blairnangle.com>
2022-11-14 15:00:47 +0000

    Complete "RNA Transcription"

---
+++ rna_transcription/rna_transcription.py
@@ -0,0 +1,7 @@
+def to_rna(dna_strand):
+    return "".join(map(lambda nucleotide: {
+        "G": "C",
+        "C": "G",
+        "T": "A",
+        "A": "U"
+    }.get(nucleotide, ""), list(dna_strand)))
