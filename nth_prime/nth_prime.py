1265f76f03c1d6f5413694268954564aa72ec1b8
Blair Nangle <hi@blairnangle.com>
2023-08-17 13:10:43 +0100

    Solve "Nth Prime" with simplified solution

--- nth_prime/nth_prime.py
+++ nth_prime/nth_prime.py
@@ -1,33 +1,27 @@
 import math
-from typing import Generator
 
 
 def calculate_upper_limit(n: int) -> [int]:
     return math.ceil(n * math.log(n * math.log(n))) + 1
 
 
-def prime_generator(limit: int) -> Generator[int, None, None]:
-    mask: [bool] = [True] * limit
+def prime_generator(n: int) -> [int]:
+    limit = n + 3 if n <= 3 else calculate_upper_limit(n)
+    mask: [bool] = [False, False] + [True] * limit
+    primes: [int] = []
     for i in range(2, limit):
         if mask[i]:
-            yield i
+            primes.append(i)
             for c in range(i * i, limit, i):
                 mask[c] = False
 
+    return primes
+
 
 def prime(n) -> int:
-    match n:
-        case 0:
-            raise ValueError("there is no zeroth prime")
-        case 1:
-            return 2
-        case 2:
-            return 3
-        case 3:
-            return 5
-        case _:
-            generator: Generator[int, None, None] = prime_generator(
-                calculate_upper_limit(n)
-            )
+    if n == 0:
+        raise ValueError("there is no zeroth prime")
+    else:
+        primes: [int] = prime_generator(n)
 
-            return next(x for i, x in enumerate(generator) if i == n - 1)
+        return primes[n - 1]
